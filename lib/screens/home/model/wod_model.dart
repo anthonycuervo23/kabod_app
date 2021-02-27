cb239817199bc9b72b3ddf19117ee4fb3d2f12b0
jean cuervo <martoranito@gmail.com>
2021-02-27 12:49:42 +0100

    Retrieving data from Firebase Firestore

--- lib/screens/home/model/wod_model.dart
+++ lib/screens/home/model/wod_model.dart
@@ -1,30 +1,22 @@
-class WodModel {
-  final String title;
-  final String type;
-  final String description;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-  WodModel({this.title, this.type, this.description});
+class Wod {
+  String title;
+  String type;
+  String description;
+  DateTime date;
+
+  Wod({this.title, this.type, this.description, this.date});
+
+  factory Wod.fromFirestore(DocumentSnapshot doc) {
+    Map data = doc.data();
+
+    return Wod(
+        title: data["wod_name"],
+        type: data["wod_type"],
+        description: data["wod_description"],
+        date: data["wod_date"].toDate());
+  }
+
+  //   date: DateTime.fromMillisecondsSinceEpoch(data['wod_date']));
 }
-
-List<WodModel> wodList = [
-  WodModel(title: 'Strength', type: 'For Time', description: '''
-Every 2 Minutes For 12 Minutes (6 Sets)
-1 Power Snatch + 1 Hang Power Snatch
-70-75% Of 1rm Snatch
-'''),
-  WodModel(
-    title: 'Murph',
-    type: 'For Time',
-    description: '''
-1 Mile Run
-100 Pull ups
-200 Push ups
-300 Squats
-1 Mile Run
-Partion the Pull ups, Push ups and Squats as needed.
-Start and finish with a mile run.
-If you have got a twenty pound vest or body armor, wear it.
-In memory of Michael Murph, who was killed in Afghanistan June 28th, 2005.
-''',
-  ),
-];
