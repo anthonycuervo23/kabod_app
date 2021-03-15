7e49d9f687aa904ff1a9d477d211d83113cf238a
jean cuervo <martoranito@gmail.com>
2021-03-15 17:43:58 +0100

    schedule model created

--- lib/core/repository/schedule_repository.dart
+++ lib/core/repository/schedule_repository.dart
@@ -9,7 +9,7 @@ class ScheduleRepository {
   ScheduleRepository(this._firestore) : assert(_firestore != null);
 
   Stream<List<Schedule>> getSchedule() {
-    return _firestore.collection('classes').snapshots().map((snapshot) {
+    return _firestore.collection('schedule').snapshots().map((snapshot) {
       return snapshot.docs
           .map(
             (document) => Schedule.fromMap(document.data(), document.id),
