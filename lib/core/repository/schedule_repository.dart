72215ab68b8271381582dd03a3fdcca898084af9
jean cuervo <martoranito@gmail.com>
2021-03-15 17:33:50 +0100

    schedule model created

---
+++ lib/core/repository/schedule_repository.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+//My imports
+import 'package:kabod_app/core/model/schedule_model.dart';
+
+class ScheduleRepository {
+  final FirebaseFirestore _firestore;
+
+  ScheduleRepository(this._firestore) : assert(_firestore != null);
+
+  Stream<List<Schedule>> getSchedule() {
+    return _firestore.collection('classes').snapshots().map((snapshot) {
+      return snapshot.docs
+          .map(
+            (document) => Schedule.fromMap(document.data(), document.id),
+          )
+          .toList();
+    });
+  }
+}
