53d425d9bc6061d1dbbfc828d2a305d144b2db4f
jean cuervo <martoranito@gmail.com>
2021-08-16 19:49:45 +0200

    added video extension .m4v

--- lib/screens/Stories/story_create.dart
+++ lib/screens/Stories/story_create.dart
@@ -165,14 +165,17 @@ class _StoryCreateState extends State<StoryCreate> {
                                               filePath: value.paths[0],
                                             )));
                               } else if (value.files.first.extension == "mp4" ||
-                                  value.files.first.extension == "mkv" || value.files.first.extension == 'mov') {
+                                  value.files.first.extension == "mkv" ||
+                                  value.files.first.extension == 'mov' ||
+                                  value.files.first.extension == 'm4v') {
                                 Navigator.push(
                                     context,
                                     MaterialPageRoute(
                                         builder: (_) => VideoStoryEditor(
                                             File(value.paths[0]))));
                               } else {
-                                print('error =====> ${value.files.first.extension}');
+                                print(
+                                    'error =====> ${value.files.first.extension}');
                                 final snackBar = SnackBar(
                                     content: Text(value.files.first.extension));
                                 ScaffoldMessenger.of(context).showSnackBar(
